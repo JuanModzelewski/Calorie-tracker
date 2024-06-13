d6ff9f275133f31132413b73aeb07f8462876224
JuanModzelewski <juanmodzelewski@gmail.com>
2024-06-13 17:00:52 +0200

    removed log to check sizing is in heroku

--- run.py
+++ run.py
@@ -49,25 +49,7 @@ def welcome_screen():
     Displays Banner and welcome message
     """
     # Logo
-    print(Fore.BLUE + r'''                                                                                                                                                                                                                                             
-              ,ad8888ba,         db         88           ,ad8888ba,    88888888ba   88  88888888888           
-             d8"'    `"8b       d88b        88          d8"'    `"8b   88      "8b  88  88                    
-            d8'                d8'`8b       88         d8'        `8b  88      ,8P  88  88                    
-            88                d8'  `8b      88         88          88  88aaaaaa8P'  88  88aaaaa               
-            88               d8YaaaaY8b     88         88          88  88""""88'    88  88"""""               
-            Y8,             d8""""""""8b    88         Y8,        ,8P  88    `8b    88  88                    
-             Y8a.    .a8P  d8'        `8b   88          Y8a.    .a8P   88     `8b   88  88                    
-              `"Y8888Y"'  d8'          `8b  88888888888  `"Y8888Y"'    88      `8b  88  88888888888           
-                                                                                                                                                                                                                                                                                                                   
-       888888888888  88888888ba          db         ,ad8888ba,   88      a8P   88888888888  88888888ba   
-            88       88      "8b        d88b       d8"'    `"8b  88    ,88'    88           88      "8b  
-            88       88      ,8P       d8'`8b     d8'            88  ,88"      88           88      ,8P  
-            88       88aaaaaa8P'      d8'  `8b    88             88,d88'       88aaaaa      88aaaaaa8P'  
-            88       88""""88'       d8YaaaaY8b   88             8888"88,      88"""""      88""""88'    
-            88       88    `8b      d8""""""""8b  Y8,            88P   Y8b     88           88    `8b    
-            88       88     `8b    d8'        `8b  Y8a.    .a8P  88     "88,   88           88     `8b   
-            88       88      `8b  d8'          `8b  `"Y8888Y"'   88       Y8b  88888888888  88      `8b                                                                                                                                                                                                                                                                                                                               
-    ''')
+    print()
 
     print(Fore.BLUE + "         WELCOME TO CALORIE TRACKER")
     print("     Your daily calorie tracking tool to ensure you meet your")
